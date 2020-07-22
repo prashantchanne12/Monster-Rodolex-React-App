2f9e7d885dfc21e0affa9f185b062a1987ba420b
prashantchanne12 <prashantchanne121@gmail.com>
2020-07-22 11:57:59 +0530

    Updates

---
+++ precache-manifest.facee83de6d3ebacc92b6bcad07fc1f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3451177222bf8c587dbc22da67fe1e37",
+    "url": "/Monster-Rodolex-React-App/index.html"
+  },
+  {
+    "revision": "18939ce0137dc4e7276a",
+    "url": "/Monster-Rodolex-React-App/static/css/main.50309ade.chunk.css"
+  },
+  {
+    "revision": "d02e0675296ee8226cd0",
+    "url": "/Monster-Rodolex-React-App/static/js/2.459ea644.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rodolex-React-App/static/js/2.459ea644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18939ce0137dc4e7276a",
+    "url": "/Monster-Rodolex-React-App/static/js/main.3d3f9ba7.chunk.js"
+  },
+  {
+    "revision": "37aca3c822f099981821",
+    "url": "/Monster-Rodolex-React-App/static/js/runtime-main.62fe4a6f.js"
+  }
+]);
